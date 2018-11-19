8fb32d78b389e7efbf6d933e17dd888d7ad74e49
Reda <reda.daugelaviciute@gmail.com>
2018-11-19 22:45:58 +0100

    First changes for jQuery Spaghetti Part made

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,6 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes.");
-  }
-}
+var mobileMenu = new MobileMenu();
 
-alert("ABC 321");
 
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
 
