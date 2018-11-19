075d441fbd13dbbc1d6109ff07056b7259b06f2f
Reda <reda.daugelaviciute@gmail.com>
2018-11-19 15:35:01 +0100

    webpack integrated to gulp automation

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 var Person = require('./modules/Person');
 
+alert("I wanna cry");
+
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
