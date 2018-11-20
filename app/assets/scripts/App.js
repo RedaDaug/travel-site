0bb43d6498ce7305978cba1ce0c1b1b16144994b
Reda <reda.daugelaviciute@gmail.com>
2018-11-20 19:53:23 +0100

    Reveal on scroll made reusable

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,12 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
+
+
 
 
 
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+	constructor(elementClass,offsetVal) {
+		this.itemsToReveal = elementClass;
+		this.offsetValue = offsetVal;
+		this.hideInitially();		
+		this.createWaypoints();		
+	}
+	
+	hideInitially(){
+		this.itemsToReveal.addClass("reveal-item");
+	}
+	
+	createWaypoints(){
+		var that = this;
+		this.itemsToReveal.each(function(){
+			var currentItem = this;
+			new Waypoint({
+				element: currentItem,    /*DOM element*/
+				handler: function() {    /*Action*/
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				offset: that.offsetValue
+			});								
+		});		
+	}
+}
+
+export default RevealOnScroll;
+
